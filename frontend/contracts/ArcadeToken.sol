10e8a134479663eed06ab1fb86db5ad30007d985
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-07-27 15:39:31 +0200

    added chainlink smartcontracts

--- frontend/contracts/ArcadeToken.sol
+++ frontend/contracts/ArcadeToken.sol
@@ -2,17 +2,39 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract ArcadeToken is ERC20, Ownable {
-    constructor() ERC20("ArcadeToken", "ARCD") Ownable(msg.sender) {}
+contract ArcadeToken is ERC20 {
+    address public feeCollector;
+    AggregatorV3Interface internal priceFeed;
+    uint256 public constant USD_FEE = 1 * 10 ** 18; // $1 in wei
 
-    // Allow anyone to mint tokens
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    constructor() ERC20("ArcadeToken", "ARCD") {
+        feeCollector = msg.sender;
+        // Address for the ETH/USD price feed on the mainnet, change if using a different network
+        priceFeed = AggregatorV3Interface(0xF79D6aFBb6dA890132F9D7c355e3015f15F3406F); 
     }
 
-    function totalSupply() public view override returns (uint256) {
-        return super.totalSupply();
+    function getLatestPrice() public view returns (uint256) {
+        (
+            , 
+            int price,
+            ,
+            ,
+            
+        ) = priceFeed.latestRoundData();
+        return uint256(price) * 10 ** 10; // Convert price to 18 decimal places
+    }
+
+    function calculateFee() public view returns (uint256) {
+        uint256 ethPrice = getLatestPrice();
+        return USD_FEE / ethPrice;
+    }
+
+    function mint(address to, uint256 amount) public payable {
+        uint256 fee = calculateFee();
+        require(msg.value >= fee, "Insufficient fee");
+        payable(feeCollector).transfer(msg.value);
+        _mint(to, amount);
     }
 }
